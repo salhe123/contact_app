6b90facc540aa9f2a13df5dcae51abc83b83501d
salhe <ibnuseid27@gmail.com>
2023-06-22 16:40:41 +0300

    day

--- src/components/login_sign/login.vue
+++ src/components/login_sign/login.vue
@@ -1,39 +1,73 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-      <h2 class="text-2xl font-bold mb-4">Login</h2>
-      <form @submit.prevent="login" class="flex flex-col space-y-4">  
-        <input type="email" id="email" placeholder="Email" v-model="email" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <input type="password" id="password" placeholder="Password" v-model="password" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-          Login
-        </button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      };
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h2 class="text-2xl font-bold mb-4">Login</h2>
+    <form @submit.prevent="login" class="flex flex-col space-y-4">  
+      <input type="email" id="email" placeholder="Email" v-model="email" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <input type="password" id="password" placeholder="Password" v-model="password" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { gql, useMutation } from '@apollo/client';
+
+const LOGIN_MUTATION = useMutation(gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        email
+        password
+      }
+    }
+  }
+`);
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const { data } = await this.$apollo.mutate({
+          mutation: LOGIN_MUTATION,
+          variables: {
+            email: this.email,
+            password: this.password,
+          },
+        });
+        localStorage.setItem('token', data.login.token);
+        localStorage.setItem('userId', data.login.user.id);
+        this.$router.push('/');
+      } catch (err) {
+        console.error(err);
+      }
     },
-    methods: {
-      login() {
-        // Perform login logic using ApolloClient or Axios
-        // Call the backend API to authenticate the user with provided email and password
-        // Handle success or failure response from the backend
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  /* Add Tailwind CSS classes here */
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+/* Add Tailwind CSS classes here */
+</style>
+```
+
+This code defines a `LOGIN_MUTATION` 
+that sends a [login request](poe://www.poe.com/_api/key_phrase?phrase=login%20request&prompt=Tell%20me%20more%20about%20login%20request.) 
+to your Hasura GraphQL API using the `login` mutation. It also defines a `login` method
+ that uses the `useMutation` hook from [Apollo Client](poe://www.poe.com/_api/key_phrase?phrase=Apollo%20Client&prompt=Tell%20me%20more%20about%20Apollo%20Client.) 
+ to perform the mutation. If the login is successful, the [JWT token](poe://www.poe.com/_api/key_phrase?phrase=JWT%20token&prompt=Tell%20me%20more%20about%20JWT%20token.) 
+ and [user ID](poe://www.poe.com/_api/key_phrase?phrase=user%20ID&prompt=Tell%20me%20more%20about%20user%20ID.) are stored in `localStorage` and the user is redirected to the home page. 
+ Note that in the `login` method, you will need to replace the `localStorage` keys with your own keys.
--- src/components/login_sign/signup.vue
+++ src/components/login_sign/signup.vue
@@ -1,46 +1,81 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-      <h2 class="text-2xl font-bold mb-4">Signup</h2>
-      <form @submit.prevent="signup" class="flex flex-col space-y-4">
-        <input type="email" id="email" placeholder="username" v-model="username" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <input type="email" id="email" placeholder="Email" v-model="email" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <input type="password" id="password" placeholder="password" v-model="password" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <input type="password" id="confirmPassword" placeholder="Confirm Password" v-model="confirmPassword" required
-          class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-          Signup
-        </button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-        confirmPassword: '',
-      };
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h2 class="text-2xl font-bold mb-4">Signup</h2>
+    <form @submit.prevent="signup" class="flex flex-col space-y-4">
+      <input type="email" id="email" placeholder="username" v-model="username" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <input type="email" id="email" placeholder="Email" v-model="email" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <input type="password" id="password" placeholder="password" v-model="password" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <input type="password" id="confirmPassword" placeholder="Confirm Password" v-model="confirmPassword" required
+        class="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+      <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+        Signup
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { gql, useMutation } from '@apollo/client';
+
+const SIGNUP_MUTATION = gql`
+  mutation Signup($email: String!, $password: String!, $username: String!) {
+    signup(email: $email, password: $password, username: $username) {
+      token
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      username: '',
+    };
+  },
+  methods: {
+    async signup() {
+      try {
+        const { data } = await this.$apollo.mutate({
+          mutation: SIGNUP_MUTATION,
+          variables: {
+            email: this.email,
+            password: this.password,
+            username: this.username,
+          },
+        });
+        localStorage.setItem('token', data.signup.token);
+        localStorage.setItem('userId', data.signup.user.id);
+        this.$router.push('/');
+      } catch (err) {
+        console.error(err);
+      }
     },
-    methods: {
-      signup() {
-        // Perform signup logic using ApolloClient or Axios
-        // Call the backend API to create a new user with provided email and password
-        // Handle success or failure response from the backend
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  /* Add Tailwind CSS classes here */
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+/* Add Tailwind CSS classes here */
+</style>
+```
+
+This code defines a `SIGNUP_MUTATION` that sends a [signup request](poe://www.poe.com/_api/key_phrase?phrase=signup%20request&prompt=Tell%20me%20more%20about%20signup%20request.)
+ to your Hasura GraphQL API using the `signup` mutation. It also defines a `signup` 
+ method that uses the `useMutation` hook from Apollo Client to perform the mutation. If the signup is successful,
+  the [JWT](poe://www.poe.com/_api/key_phrase?phrase=JWT&prompt=Tell%20me%20more%20about%20JWT.) token and user 
+  ID are 
+  stored in `localStorage` and the user is redirected to the home page. 
+Note that in the `signup` method, you will need to replace the `localStorage` keys with your own keys.
